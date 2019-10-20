c2ca39909ff193f8fd37348a26416dcf0fb50f8a
Rommy Gustiawan <rgcppp@gmail.com>
2019-10-20 21:47:34 +0700

    Move the form left & the result to right

--- index.php
+++ index.php
@@ -21,55 +21,60 @@
 
     <div class="container">
         <?php $result = $mysqli->query("SELECT * FROM coba") or die($mysqli->error); ?>
-        <div class="row justify-content-center">
-            <form action="config.php" method="POST">
-                <input type="hidden" name="id" value="<?php echo $id; ?>">
-                <div class="form-group">
-                    <label>Mata Kuliah</label>
-                    <input type="text" class="form-control" name="matkul" value="<?php echo $matkul; ?>" placeholder="Mata kuliah apa?">
+        <div class="row">
+            <div class="col-sm">
+                <div class="row justify-content-center">
+                    <form action="config.php" method="POST">
+                        <input type="hidden" name="id" value="<?php echo $id; ?>">
+                        <div class="form-group">
+                            <label>Mata Kuliah</label>
+                            <input type="text" class="form-control" name="matkul" value="<?php echo $matkul; ?>" placeholder="Mata kuliah apa?">
+                        </div>
+                        <div class="form-group">
+                            <label>Tugas</label>
+                            <input type="text" class="form-control" name="tugas" value="<?php echo $tugas; ?>" placeholder="Tugas-nya ngapain?">
+                        </div>
+                        <div class="form-group">
+                            <label>Deadline</label>
+                            <input type="text" class="form-control" name="deadline" value="<?php echo $deadline; ?>" placeholder="Kapan dikumpulin?">
+                        </div>
+                        <div class="form-group">
+                        <?php if ($update == true): ?>
+                            <button type="submit" class="btn btn-info" name="update">Update Tugas</button>
+                        <?php else: ?>
+                            <button type="submit" class="btn btn-primary" name="save">Simpan Tugas</button>
+                        <?php endif; ?>
+                        </div>
+                    </form>
                 </div>
-                <div class="form-group">
-                    <label>Tugas</label>
-                    <input type="text" class="form-control" name="tugas" value="<?php echo $tugas; ?>" placeholder="Tugas-nya ngapain?">
+            </div>
+            <div class="col-sm">
+                <div class="row justify-content-center">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Mata Kuliah</th>
+                                <th>Tugas</th>
+                                <th>Deadline</th>
+                                <th colspan="2">Action</th>
+                            </tr>
+                        </thead>
+                        <?php while ($row = $result->fetch_assoc()): ?>
+                            <tr>
+                                <td><?php echo $row['matkul']; ?></td>
+                                <td><?php echo $row['tugas']; ?></td>
+                                <td><?php echo $row['deadline']; ?></td>
+                                <td>
+                                    <a href="index.php?edit=<?php echo $row['id']; ?>"
+                                        class="btn btn-info">Ubah</a>
+                                    <a href="config.php?delete=<?php echo $row['id']; ?>"
+                                        class="btn btn-danger">Selesai</a>
+                                </td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </table>
                 </div>
-                <div class="form-group">
-                    <label>Deadline</label>
-                    <input type="text" class="form-control" name="deadline" value="<?php echo $deadline; ?>" placeholder="Kapan dikumpulin?">
-                </div>
-                <div class="form-group">
-                <?php if ($update == true): ?>
-                    <button type="submit" class="btn btn-info" name="update">Update Tugas</button>
-                <?php else: ?>
-                    <button type="submit" class="btn btn-primary" name="save">Simpan Tugas</button>
-                <?php endif; ?>
-                </div>
-            </form>
-        </div>
-        
-        <div class="row justify-content-center">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Mata Kuliah</th>
-                        <th>Tugas</th>
-                        <th>Deadline</th>
-                        <th colspan="2">Action</th>
-                    </tr>
-                </thead>
-                <?php while ($row = $result->fetch_assoc()): ?>
-                    <tr>
-                        <td><?php echo $row['matkul']; ?></td>
-                        <td><?php echo $row['tugas']; ?></td>
-                        <td><?php echo $row['deadline']; ?></td>
-                        <td>
-                            <a href="index.php?edit=<?php echo $row['id']; ?>"
-                                class="btn btn-info">Ubah</a>
-                            <a href="config.php?delete=<?php echo $row['id']; ?>"
-                                class="btn btn-danger">Selesai</a>
-                        </td>
-                    </tr>
-                <?php endwhile; ?>
-            </table>
+            </div>
         </div>
     </div>
 </body>
