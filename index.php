9631a59c0b7fbef10297be1f85fa472ce63c0328
Rommy Gustiawan <rgcppp@gmail.com>
2019-11-24 13:16:54 +0700

    Add fontawesome CDN

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="fontawesome/css/all.min.css">
+        <script src="https://kit.fontawesome.com/31372f9642.js" crossorigin="anonymous"></script>
         <title>Catat Tugas</title>
     </head>
     <body>
