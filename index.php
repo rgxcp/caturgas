81ed5ffd6101e5852d3ad6ffee9944c44ccff96c
Rommy Gustiawan <rgcppp@gmail.com>
2019-11-26 07:31:14 +0700

    Move script to <head>

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/31372f9642.js" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="script.js"></script>
         <style>
             .customButton {
                 margin: 1.5em 0.6em;
@@ -96,7 +98,5 @@
                 </table>
             </div>
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="script.js"></script>
     </body>
 </html>
